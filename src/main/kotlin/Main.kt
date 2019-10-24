51df4cbbadb8353bbd2ddfdfff018e5c169b4b3f
rolo7 <50997752+rolo7@users.noreply.github.com>
2019-10-23 19:51:07 -0600

    commit final este si

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,17 +4,17 @@ val sitio = classLugar()
 fun main(args: Array<String>)
 {
 
-    val ingresePiso:Int
+    val ingresePiso:Int?
 
     println("Ingrese el piso al que desea ir: ")
-    ingresePiso = readLine().toInt() as Int
-    ingresePiso.toInt() as Int
+    ingresePiso = readLine()?.toInt() as Int
     while(ingresePiso<15)
     {
-        thread.sleep(1000)
+
         println("El Elevador se encuentra detenido en el Lobby")
         print(estado)
         println(sitio)
+
         if(ingresePiso==sitio)
         {
             println("El elevador a llegado al piso indicado")
