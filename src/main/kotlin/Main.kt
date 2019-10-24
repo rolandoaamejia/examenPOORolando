91a68ccdfc0c6d9ce9216b566d9b6d1baa5cff0c
rolo7 <50997752+rolo7@users.noreply.github.com>
2019-10-23 18:54:06 -0600

    lugar donde se encuentra el elevador y estado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 val estado = classElevador()
+val sitio = classLugar()
 fun main(args: Array<String>)
 {
+    println("El Elevador se encuentra detenido en el Lobby")
    print(estado)
-
+   println(sitio)
 }
--- src/main/kotlin/classLugar.kt
+++ src/main/kotlin/classLugar.kt
@@ -1,2 +1,12 @@
-class classLugar {
+import java.util.*
+
+open class classLugar(var piso : Int =(random.nextInt(15)+1))
+{
+    override fun toString(): String {
+
+        return " en el piso $piso"
+
+    }
+
+
 }
